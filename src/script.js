7acc27ae1a57ea73de791afec14749e2419643aa
Irina <zaletk.irina@gmail.com>
2019-11-03 17:54:18 +0300

    init: start codejam-virtual-keyboard task and feat: add basic page layout

---
+++ src/script.js
@@ -0,0 +1,36 @@
+/* eslint-disable no-unused-vars */
+import './main.css';
+import kbdkeys from './keys';
+
+global.console.log(kbdkeys);
+
+const createEl = (tag, cls, addTo, tagvalue) => {
+  const el = document.createElement(tag);
+  if (cls != null) {
+    el.setAttribute('class', cls);
+  }
+  if (tagvalue != null) {
+    el.innerText = tagvalue;
+  }
+
+  addTo.append(el);
+  return el;
+};
+
+const init = () => {
+  const body = document.querySelector('body');
+  const root = createEl('div', 'root', body, null);
+  const wrapper = createEl('div', 'wrapper', root, null);
+  const h1 = createEl('h1', null, wrapper, 'Virtual keyboard');
+  const textArea = createEl('textarea', null, wrapper, null);
+  const kbdContainer = createEl('div', 'keyboard', wrapper, null);
+
+  kbdkeys.forEach((row) => {
+    const sectionRow = createEl('section', 'key-row', kbdContainer, null);
+    row.forEach((keyItem) => {
+      const keyDiv = createEl('div', `key ${keyItem.className[0]}`, sectionRow, keyItem.name);
+    });
+  });
+};
+
+init();
